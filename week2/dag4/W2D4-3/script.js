8d8c7d5f2fde99b5d1bee800f5bb9a31af66ccdc
Rahied Ishaak <rahied.ishaak.code@gmail.com>
2019-10-10 12:52:16 +0200

    adds opdracht W2D4-3

---
+++ week2/dag4/W2D4-3/script.js
@@ -0,0 +1,10 @@
+// zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = function () {
+//     console.log("Joe, ik rock de arrow functions!");
+// };
+
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+  
+ikRockArrowFunctions();
